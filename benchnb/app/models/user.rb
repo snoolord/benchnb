f8b6032b92c8e6c318c151b371548d44e2db5267
Winston <winsotn.j.zhao@gmail.com>
2016-10-28 14:46:45 -0700

    creating user works

--- benchnb/app/models/user.rb
+++ benchnb/app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   after_initialize :ensure_session_token
 
   attr_reader :password
-  
+
   def self.find_by_credentials(username, password)
     @user = User.find_by(username: username)
     return nil unless @user && @user.is_password?(password)
@@ -13,5 +13,22 @@ class User < ActiveRecord::Base
   end
 
   def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_passwword?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  private
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
   end
 end
